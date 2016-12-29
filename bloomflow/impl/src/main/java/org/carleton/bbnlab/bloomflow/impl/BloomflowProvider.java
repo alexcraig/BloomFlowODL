d1251de302eeec74922604677e29d894fcad1c68
Alexander Craig <aaecraig@connect.carleton.ca>
2016-12-29 10:14:35 -0800

    Modified project to load openflow southbound plugin as dependency

--- bloomflow/impl/src/main/java/org/carleton/bbnlab/bloomflow/impl/BloomflowProvider.java
+++ bloomflow/impl/src/main/java/org/carleton/bbnlab/bloomflow/impl/BloomflowProvider.java
@@ -8,6 +8,8 @@
 package org.carleton.bbnlab.bloomflow.impl;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,15 +18,19 @@ public class BloomflowProvider {
     private static final Logger LOG = LoggerFactory.getLogger(BloomflowProvider.class);
 
     private final DataBroker dataBroker;
+    private final NotificationPublishService notificationService;
 
-    public BloomflowProvider(final DataBroker dataBroker) {
+    public BloomflowProvider(final DataBroker dataBroker,
+            final NotificationPublishService notificationService) {
         this.dataBroker = dataBroker;
+        this.notificationService = notificationService;
     }
 
     /**
      * Method called when the blueprint container is created.
      */
     public void init() {
+        // this.notificationService.registerInterestListener(this); // Deprecated method
         LOG.info("BloomflowProvider Session Initiated");
     }
 
